ea0d2e7765684cec87bb6c28772ac51bb412ea8e
yangqi <yangqi_dream@163.com>
2026-05-08 23:00:44 +0800

    feat: add wechat account repository

---
+++ internal/repository/user/wechat_account_repo_test.go
@@ -0,0 +1,72 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/glebarez/sqlite"
+	"gorm.io/gorm"
+	domain "wechat-intelligent-bot/internal/domain/user"
+)
+
+func setupWechatTestDB(t *testing.T) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	err = db.AutoMigrate(&domain.User{}, &domain.WechatAccount{})
+	assert.NoError(t, err)
+
+	return db
+}
+
+func TestWechatAccountRepository_Create(t *testing.T) {
+	db := setupWechatTestDB(t)
+	repo := NewWechatAccountRepository(db)
+
+	// 先创建用户
+	u := domain.NewUser()
+	_ = db.Create(u).Error
+
+	account := domain.NewWechatAccount(u.ID, "test_openid_123")
+	err := repo.Create(account)
+
+	assert.NoError(t, err)
+	assert.Greater(t, account.ID, int64(0))
+}
+
+func TestWechatAccountRepository_GetByOpenID(t *testing.T) {
+	db := setupWechatTestDB(t)
+	repo := NewWechatAccountRepository(db)
+
+	u := domain.NewUser()
+	_ = db.Create(u).Error
+
+	account := domain.NewWechatAccount(u.ID, "test_openid_123")
+	_ = repo.Create(account)
+
+	found, err := repo.GetByOpenID("test_openid_123")
+
+	assert.NoError(t, err)
+	assert.NotNil(t, found)
+	assert.Equal(t, "test_openid_123", found.OpenID)
+	assert.Equal(t, u.ID, found.UserID)
+}
+
+func TestWechatAccountRepository_GetByUnionID(t *testing.T) {
+	db := setupWechatTestDB(t)
+	repo := NewWechatAccountRepository(db)
+
+	u := domain.NewUser()
+	_ = db.Create(u).Error
+
+	account := domain.NewWechatAccount(u.ID, "test_openid_123")
+	unionID := "test_union_456"
+	account.SetUnionID(unionID)
+	_ = repo.Create(account)
+
+	found, err := repo.GetByUnionID(unionID)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, found)
+	assert.Equal(t, unionID, *found.UnionID)
+}
